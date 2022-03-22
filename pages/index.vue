b6e180a16649242635e859b692536c48bae6b683
Nattapat Niyomthai <nattapat.niy@gmail.com>
2022-03-22 16:57:57 +0700

    feat: add download button

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <Tutorial />
+  <div class="container pt-5"> 
+    <div class="row pt-5">
+      <div class="center pointer" @click="downloadCsv()">
+        <button type="button" class="btn btn-success">Download CSV</button>
+        <img src="~assets/image/csv-icon.png" alt="" style="width: 30px;">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  methods:{
+    downloadCsv(){
+      console.log("download csv")
+    }
+  }
 }
 </script>
+
+<style scoped>
+.center {
+  margin: auto;
+}
+.pointer {
+  cursor: pointer;
+}
+</style>
